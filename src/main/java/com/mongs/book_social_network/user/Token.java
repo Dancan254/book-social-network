e6ecd4a4fcaafebef7653f039f1a09a236f873cc
Dancan254 <dancanian25@gmail.com>
2024-12-02 23:21:17 +0300

    Add Token entity with JPA annotations and auditing fields

---
+++ src/main/java/com/mongs/book_social_network/user/Token.java
@@ -0,0 +1,28 @@
+package com.mongs.book_social_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class Token {
+    @Id
+    @GeneratedValue
+    private Long tokenId;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+}
