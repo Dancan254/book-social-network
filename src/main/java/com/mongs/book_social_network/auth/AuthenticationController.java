fa00124bcb7701af79a44c7bfaf7f8d40b6894dc
Dancan254 <dancanian25@gmail.com>
2024-12-09 19:28:36 +0300

    Add AuthenticationController for user registration endpoint

---
+++ src/main/java/com/mongs/book_social_network/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.mongs.book_social_network.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Authentication API")
+public class AuthenticationController {
+    private AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED) 
+    public ResponseEntity<?> register(@RequestBody RegistrationRequest request) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
