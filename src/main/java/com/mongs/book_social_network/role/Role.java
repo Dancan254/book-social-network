ad8941fc59404efada32ba280191d344a6ba945b
Dancan254 <dancanian25@gmail.com>
2024-12-11 22:37:32 +0300

    Add creation timestamp to Role entity for auditing purposes

--- src/main/java/com/mongs/book_social_network/role/Role.java
+++ src/main/java/com/mongs/book_social_network/role/Role.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mongs.book_social_network.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,13 +22,16 @@ public class Role {
     @Id
     @GeneratedValue
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     @ManyToMany(mappedBy = "roles")
     @JsonIgnore
     private List<User> user;
 
     @CreatedDate
+    @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
 
