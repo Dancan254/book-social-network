8577bc7901a358226c057130345a4b1cfde5f580
Dancan254 <dancanian25@gmail.com>
2024-12-14 19:52:54 +0300

    Add activateAccount endpoint to AuthenticationController for account activation via token

--- src/main/java/com/mongs/book_social_network/auth/AuthenticationController.java
+++ src/main/java/com/mongs/book_social_network/auth/AuthenticationController.java
@@ -2,9 +2,11 @@ package com.mongs.book_social_network.auth;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +35,9 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.login(request));
     }
     
+    @GetMapping("/activate-account")
+    public void activateAccount(@RequestParam String token) throws MessagingException {
+        service.activateAccount(token);
+        
+    }
 }
