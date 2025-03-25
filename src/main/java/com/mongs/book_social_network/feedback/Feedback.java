03fde41091ed44968491dfe7c7e0c57479456842
Dancan254 <dancanian25@gmail.com>
2025-03-25 21:45:16 +0300

    feat: add Feedback entity with score, comment, and relationship to Book

---
+++ src/main/java/com/mongs/book_social_network/feedback/Feedback.java
@@ -0,0 +1,25 @@
+package com.mongs.book_social_network.feedback;
+
+import com.mongs.book_social_network.book.Book;
+import com.mongs.book_social_network.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class Feedback extends BaseEntity {
+    private Double score;
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+}
