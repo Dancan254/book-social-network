a775f48f8d509a75c5b8431a827257ac7cd51a57
Dancan254 <dancanian25@gmail.com>
2025-03-26 06:36:34 +0300

    feat: add methods to find book by ID and retrieve all displayable books with pagination

--- src/main/java/com/mongs/book_social_network/services/BookService.java
+++ src/main/java/com/mongs/book_social_network/services/BookService.java
@@ -2,12 +2,20 @@ package com.mongs.book_social_network.services;
 
 import com.mongs.book_social_network.book.Book;
 import com.mongs.book_social_network.book.BookRequest;
+import com.mongs.book_social_network.book.BookResponse;
+import com.mongs.book_social_network.book.PageResponse;
 import com.mongs.book_social_network.config.BookMapper;
 import com.mongs.book_social_network.repository.BookRepository;
 import com.mongs.book_social_network.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +29,28 @@ public class BookService {
         Book book = bookMapper.toBook(request);
         book.setOwner(user);
         return bookRepository.save(book).getId();
+    }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new IllegalArgumentException("Book not found"));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication authenticatedUser) {
+        User user = (User) authenticatedUser.getPrincipal();
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> bookResponses = books.stream().map(bookMapper::toBookResponse).toList();
+        return new PageResponse<>(
+                bookResponses,
+                books.getNumber(),
+                books.getSize(),
+                (int)books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
