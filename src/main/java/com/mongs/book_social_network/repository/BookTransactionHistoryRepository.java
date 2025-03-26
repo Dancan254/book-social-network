0bb184537dc2abb93ac8cda5fa840a5f41dd010e
Dancan254 <dancanian25@gmail.com>
2025-03-26 15:55:45 +0300

    feat: add method to check if a book is already borrowed by a user

--- src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
+++ src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
@@ -18,6 +18,16 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             SELECT history
             FROM BookTransactionHistory history
             WHERE history.book.owner.id = :id
-            """)
+           """)
     Page<BookTransactionHistory> findAllReturnedBooks(Integer id, Pageable pageable);
+
+    @Query("""
+           SELECT 
+           (COUNT(*) > 0) AS isBorrowed
+           FROM BookTransactionHistory bookTransactionHistory
+           WHERE bookTransactionHistory.user.id = :userId
+           AND bookTransactionHistory.book.id = :bookId
+           AND bookTransactionHistory.returnApproved = false
+           """)
+    boolean isAlreadyBorrowed(Integer bookId, Integer userId);
 }
