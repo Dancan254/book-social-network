ae505f11790853f91b8995f778f40fda320fe285
Dancan254 <dancanian25@gmail.com>
2025-03-26 09:30:29 +0300

    feat: add ExceptionResponse class for standardized error handling

---
+++ src/main/java/com/mongs/book_social_network/exceptions/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.mongs.book_social_network.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+
+    private Integer businessCode;
+    private String errorBusinessDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
