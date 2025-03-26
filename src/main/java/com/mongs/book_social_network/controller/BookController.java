4c521753feafe694c6063d6927bdd4c618152c34
Dancan254 <dancanian25@gmail.com>
2025-03-26 06:36:28 +0300

    feat: add endpoints to retrieve book by ID and list all books with pagination

--- src/main/java/com/mongs/book_social_network/controller/BookController.java
+++ src/main/java/com/mongs/book_social_network/controller/BookController.java
@@ -1,15 +1,14 @@
 package com.mongs.book_social_network.controller;
 
 import com.mongs.book_social_network.book.BookRequest;
+import com.mongs.book_social_network.book.BookResponse;
+import com.mongs.book_social_network.book.PageResponse;
 import com.mongs.book_social_network.services.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -21,5 +20,17 @@ public class BookController {
     @PostMapping
     public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication authenticatedUser) {
         return ResponseEntity.ok(service.saveBook(request, authenticatedUser));
+    }
+
+    @GetMapping("{bookId}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable Integer bookId) {
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(@RequestParam(defaultValue = "0") Integer page,
+                                                                   @RequestParam(defaultValue = "10") Integer size,
+                                                                   Authentication authenticatedUser) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, authenticatedUser));
     }
 }
