6a5ef91479607dca26c92064753f78c933809b8a
Dancan254 <dancanian25@gmail.com>
2025-03-27 22:51:25 +0300

    feat: add method to find active book transactions by book ID and user ID

--- src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
+++ src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
     @Query("""
             SELECT history
@@ -22,7 +24,7 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
     Page<BookTransactionHistory> findAllReturnedBooks(Integer id, Pageable pageable);
 
     @Query("""
-           SELECT 
+           SELECT
            (COUNT(*) > 0) AS isBorrowed
            FROM BookTransactionHistory bookTransactionHistory
            WHERE bookTransactionHistory.user.id = :userId
@@ -30,4 +32,13 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
            AND bookTransactionHistory.returnApproved = false
            """)
     boolean isAlreadyBorrowed(Integer bookId, Integer userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory transaction
+            WHERE transaction.book.id = :bookId
+            AND transaction.user.id = :userId
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
 }
