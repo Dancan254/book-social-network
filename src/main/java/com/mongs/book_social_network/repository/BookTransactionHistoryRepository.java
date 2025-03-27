0b10f0d02ea3caac68d06eb27ffe4b7243ff91c7
Dancan254 <dancanian25@gmail.com>
2025-03-27 23:04:57 +0300

    feat: add method to find unapproved returned book transactions by owner ID

--- src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
+++ src/main/java/com/mongs/book_social_network/repository/BookTransactionHistoryRepository.java
@@ -38,7 +38,18 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             FROM BookTransactionHistory transaction
             WHERE transaction.book.id = :bookId
             AND transaction.user.id = :userId
+            AND transaction.returned = false
             AND transaction.returnApproved = false
             """)
     Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory transaction
+            WHERE transaction.book.owner.id = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = true
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(Integer bookId, Integer id);
 }
