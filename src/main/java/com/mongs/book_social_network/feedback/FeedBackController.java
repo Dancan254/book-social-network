bebb33de1f599515e000fc26f746153929196d45
Dancan254 <dancanian25@gmail.com>
2025-04-01 16:30:23 +0300

    feat: add endpoint to retrieve feedback by book ID with pagination support

--- src/main/java/com/mongs/book_social_network/feedback/FeedBackController.java
+++ src/main/java/com/mongs/book_social_network/feedback/FeedBackController.java
@@ -1,14 +1,12 @@
 package com.mongs.book_social_network.feedback;
 
+import com.mongs.book_social_network.book.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/feedback")
@@ -21,5 +19,15 @@ public class FeedBackController {
     @PostMapping()
     public ResponseEntity<Integer> saveFeedBack(@Valid @RequestBody FeedBackRequest request, Authentication authentication) {
         return ResponseEntity.ok(feedBackService.saveFeedBack(request, authentication));
+    }
+
+    @GetMapping("/book/{bookId}")
+    public ResponseEntity<PageResponse<FeedbackResponse>> getFeedBackByBookId(
+            @PathVariable Integer bookId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication authentication
+    ) {
+        return ResponseEntity.ok(feedBackService.getAllFeedbackByBookId(bookId, page, size, authentication));
     }
 }
