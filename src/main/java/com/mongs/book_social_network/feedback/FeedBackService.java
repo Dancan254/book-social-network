dc4b089d315c8d50c7c17c934bd7a55f8f0c4232
Dancan254 <dancanian25@gmail.com>
2025-04-01 16:30:47 +0300

    feat: add method to retrieve paginated feedback by book ID

--- src/main/java/com/mongs/book_social_network/feedback/FeedBackService.java
+++ src/main/java/com/mongs/book_social_network/feedback/FeedBackService.java
@@ -1,15 +1,19 @@
 package com.mongs.book_social_network.feedback;
 
 import com.mongs.book_social_network.book.Book;
+import com.mongs.book_social_network.book.PageResponse;
 import com.mongs.book_social_network.exceptions.OperationNotPermitted;
 import com.mongs.book_social_network.repository.BookRepository;
 import com.mongs.book_social_network.user.User;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -30,5 +34,25 @@ public class FeedBackService {
         }
         Feedback feedback = feedbackMapper.toFeedback(request);
         return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> getAllFeedbackByBookId(Integer bookId, int page, int size, Authentication authentication) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        User user = (User) authentication.getPrincipal();
+        Page<Feedback> feedbackPage = feedbackRepository.findAllByBookId(bookId, pageRequest);
+
+        List<FeedbackResponse> feedbackResponses = feedbackPage.getContent().stream()
+                .map(feedback -> feedbackMapper.toFeedbackResponse(feedback, user.getId()))
+                .toList();
+
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbackPage.getNumber(),
+                feedbackPage.getSize(),
+                (int) feedbackPage.getTotalElements(),
+                feedbackPage.getTotalPages(),
+                feedbackPage.isFirst(),
+                feedbackPage.isLast()
+        );
     }
 }
