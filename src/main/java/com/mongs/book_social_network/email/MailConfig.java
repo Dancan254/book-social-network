8e61f61f02af293d2bbd14aaefc5a930558851a4
Dancan254 <dancanian25@gmail.com>
2025-04-03 11:17:45 +0300

    feat: update MailConfig to use externalized mail properties for better configuration management

--- src/main/java/com/mongs/book_social_network/email/MailConfig.java
+++ src/main/java/com/mongs/book_social_network/email/MailConfig.java
@@ -1,5 +1,6 @@
 package com.mongs.book_social_network.email;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailSender;
@@ -11,13 +12,22 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("localhost");
-        mailSender.setPort(1025);
-        mailSender.setUsername("username");
-        mailSender.setPassword("password");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
